b81d6fa433e0221acb9faf2b996200b3ce063dd4
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-12 06:11:31 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,12 +4,12 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix';
 
 const TIMER_PERIOD = 1000;
-const start = document.querySelector('[data-start]');
-const dateText = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const dateInput = document.querySelector('#datetime-picker');
 const valueRefs = getTimerValueRefs();
 
-start.disabled = true;
-start.addEventListener('click', onStartClick);
+startBtn.disabled = true;
+startBtn.addEventListener('click', onStartClick);
 
 //
 // Инициализируем flatpickr
@@ -26,14 +26,14 @@ function onDatePickerClose(selectedDates) {
   const date = selectedDates[0];
   const isValidDate = date - Date.now() > 0;
 
-  start.disabled = !isValidDate;
+  startBtn.disabled = !isValidDate;
 
   if (!isValidDate) Notify.failure('Please choose a date in the future');
 }
 
 function onStartClick({ currentTarget: btn }) {
   // отключаем кнопку и поле
-  dateText.disabled = btn.disabled = true;
+  dateInput.disabled = btn.disabled = true;
 
   // обновляем разметку и запускаем таймер
   onTimerTick();
@@ -43,7 +43,7 @@ function onStartClick({ currentTarget: btn }) {
    * Вызывается по тику таймера
    */
   function onTimerTick() {
-    const dateDiff = Date.parse(dateText.value) - Date.now();
+    const dateDiff = Date.parse(dateInput.value) - Date.now();
 
     // обновляем значения таймера на странице
     updateTimerValues(dateDiff, valueRefs);
@@ -51,7 +51,7 @@ function onStartClick({ currentTarget: btn }) {
     // отсчет закончен
     if (dateDiff < TIMER_PERIOD) {
       // включаем кнопку и поле
-      dateText.disabled = btn.disabled = false;
+      dateInput.disabled = btn.disabled = false;
       clearInterval(timerId);
       Notify.success('Bingo!');
     }
