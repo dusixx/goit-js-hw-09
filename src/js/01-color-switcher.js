9969c2671ba7f6851eeaa59540fe7e65f33e4c24
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-12 17:00:09 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,20 +6,23 @@ const stopBtn = document.querySelector('[data-stop]');
 let timerId;
 
 stopBtn.disabled = true;
-
 startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
 
-stopBtn.addEventListener('click', ({ currentTarget: btn }) => {
-  // отключаем stopBtn, включаем startBtn
-  btn.disabled = !(startBtn.disabled = false);
-  clearInterval(timerId);
-
-  // возвращаем дефолтный цвет
-  utils.setBodyBgColor(null);
-});
+/* 
+  Обработчики событий
+*/
 
 function onStartClick({ currentTarget: btn }) {
   // отключаем startBtn, включаем stopBtn
   btn.disabled = !(stopBtn.disabled = false);
   timerId = setInterval(() => utils.setBodyBgColor(), TIMER_PERIOD);
+}
+
+function onStopClick({ currentTarget: btn }) {
+  // отключаем stopBtn, включаем startBtn
+  btn.disabled = !(startBtn.disabled = false);
+  clearInterval(timerId);
+  // возвращаем дефолтный цвет
+  utils.setBodyBgColor(null);
 }
