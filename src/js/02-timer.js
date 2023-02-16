8d89f5754339f3a079e83032272adfb47eaef644
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-16 16:22:31 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import utils from './utils';
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/dark.css';
 
 const TIMER_PERIOD = 1000;
 const startBtn = document.querySelector('[data-start]');
